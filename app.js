7002eacf00f45de35626ef3d88c287f0d0e0ad05
tacyarg <tacyarg@d28-23-14-245.dim.wideopenwest.com>
2017-10-05 16:44:28 -0500

    better rate limits

--- app.js
+++ app.js
@@ -15,13 +15,26 @@ request({
 }).then(body => {
     var $ = cheerio.load(body);
 
+    function downloadFile (url, filename) {
+        return request(url).pipe(fs.createWriteStream(path.resolve('emojis/'+filename)))
+    }
+
+    var list = []
     $('li .emoji').each(function(index) {
-        console.log( index + ": " + $( this ).attr('title') );
+        // console.log( index + ": " + $( this ).attr('title') );
         var name = $( this ).attr('title')
         var url = $( this ).find('a').attr('href')
         var filename = $( this ).find('a').attr('download')
-        console.log(url)
-        request(url).pipe(fs.createWriteStream(path.resolve('emojis/'+filename)))
+        // console.log(url)
+        list.push({ name, url, filename })
+    })
+
+    console.time('Downloads Complete.')
+    return Promise.map(list, item => {
+        console.log(JSON.stringify(item))
+        return Promise.resolve(downloadFile(item.url, item.filename)).delay(250)
+    }, {concurrency: 10}).finally(function(){
+        console.timeEnd('Downloads Complete.')
     })
     
 })

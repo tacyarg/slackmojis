9c3e0bf089817061f26c77154d5f172c16773696
Adam Gall <adam@decentlabs.io>
2019-05-24 11:06:59 -0400

    Rewrite the logic in app.js to download emojis sequentially

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const request = require("request-promise");
+const request = require("request-promise-native");
 const cheerio = require('cheerio');
 const cloudscraper = require('cloudscraper');
 const fs = require('fs');
@@ -17,27 +17,26 @@ request({
     json: false
 }).then(body => {
     var $ = cheerio.load(body);
-
-    function downloadFile (url, filename) {
-        return request(url).pipe(fs.createWriteStream(`${dir}/${filename}`))
-    }
-
     var list = []
     $('li .emoji').each(function(index) {
-        // console.log( index + ": " + $( this ).attr('title') );
         var name = $( this ).attr('title')
         var url = $( this ).find('a').attr('href')
         var filename = $( this ).find('a').attr('download')
-        // console.log(url)
         list.push({ name, url, filename })
     })
-
-    console.time('Downloads Complete.')
-    return Promise.mapSeries(list, item => {
-        console.log(JSON.stringify(item))
-        return downloadFile(item.url, item.filename)
-    }).finally(function(){
-        console.timeEnd('Downloads Complete.')
-    })
-    
+    return list;
+}).then(list => {
+    console.log("got the list, starting to download")
+    return list.reduce((p, item) => {
+        return p.then(() => {
+            console.log("downloading", item.filename)
+            return new Promise((resolve) => {
+                let file = fs.createWriteStream(`${dir}/${item.filename}`);
+                file.on('finish', () => resolve());
+                return request(item.url).pipe(file)
+            })
+        });
+    }, Promise.resolve());
+}).then(() => {
+    console.log("all done")
 })

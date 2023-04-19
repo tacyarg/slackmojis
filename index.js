8161d3dec2d825a990227e1b5610508729a0040c
Jacob Dent <jdent@tacyarg.com>
2023-04-19 13:40:35 -0500

    replace request with axios, handle retries for failed downloads, save metadata

--- index.js
+++ index.js
@@ -1,66 +1,139 @@
-const request = require("request-promise-native");
+require("dotenv").config();
+// const request = require("request-promise-native");
 const fs = require("fs");
 const path = require("path");
 const Promise = require("bluebird");
+const url = require("url");
+const assert = require("assert");
+
+function parseFilename(adr) {
+  var q = url.parse(adr, true);
+  const paths = q.pathname.split("/");
+  return paths[paths.length - 1];
+}
 
 var dir = path.resolve("emojis");
 if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 
+const axios = require("axios");
+
 function DownloadFile(name, url) {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
+    const cancelTimer = setTimeout(() => {
+      reject(new Error(`${name} failed to download TIMEOUT.`));
+    }, 5000);
+
     let file = fs.createWriteStream(`${dir}/${name}`);
     file.on("finish", () => {
+      clearTimeout(cancelTimer);
       console.log("Downloaded:", name);
       return resolve();
     });
-    return request(url).pipe(file);
+
+    file.on("error", (e) => {
+      clearTimeout(cancelTimer);
+      reject(e);
+    });
+
+    // stream file to disk.
+    // return request(url).catch(reject).pipe(file);
+    return axios({ url, responseType: "stream" })
+      .catch((e) => {
+        clearTimeout(cancelTimer);
+        reject(e);
+      })
+      .then((res) => res.data.pipe(file));
   });
 }
 
-
-
-const getPage = (index=0) => request({
-  url: `https://slackmojis.com/emojis.json?page=${index}`,
-  json: true
-})
-
-
-function DownloadImages (list) {
-  let count = 0;
-  let failures = 0;
-    
-  return Promise.map(
-      list,
-      async ({ name, image_url }) => {
-        await DownloadFile(name, image_url).catch(e => {
-          ++failures;
-          console.log("failed to download:", name, image_url);
-        });
-
-        console.log(`${list.length - ++count} / ${list.length} remaining...`);
-      },
-      { concurrency: process.env.CONCURRENCY || 5 }
-    );
+async function downloadFileRetry(filename, url, count = 0) {
+  return DownloadFile(filename, url).catch((e) => {
+    ++count;
+    assert(count <= 5, "max retries.");
+    console.error(e);
+    console.log("failed to download:", filename, url);
+    return downloadFileRetry(filename, url, count);
+  });
 }
 
+const getPage = (index = 0) => {
+  return axios({
+    url: `https://slackmojis.com/emojis.json?page=${index}`,
+    // json: true,
+  }).then((response) => response.data);
+}
+
+function DownloadImages(list) {
+  let count = 0;
+  let failures = 0;
+
+  return Promise.map(
+    list,
+    async ({ name, image_url, category }) => {
+      const filename = parseFilename(image_url);
+
+      await downloadFileRetry(filename, image_url);
+
+      console.log(`${list.length - ++count} / ${list.length} ${filename}...`);
+
+      const metadata = {
+        filename,
+        name,
+        image_url,
+        slug: `:${name}:`,
+        category: category?.name || null,
+      };
+
+      // // write JSON string to a file
+      // fs.appendFile(
+      //   "metadata.json",
+      //   JSON.stringify(metadata, null, 2),
+      //   (err) => {
+      //     if (err) {
+      //       throw err;
+      //     }
+      //     console.log("JSON data is saved.");
+      //   }
+      // );
+
+      return metadata;
+    },
+    { concurrency: Number(process.env.CONCURRENCY) || 25 }
+  );
+}
 
 // recursively download each page until no more are found.
 // could buffer results in memory, however this method is more efficient
 function Run() {
-
   async function ProcessPage(index = 0) {
-    console.log("Requesting Page:", index);
-    
-    const list = await getPage(index)
-    if(!list.length) return
+    console.log("> PROCESSING PAGE:", index);
 
-    await DownloadImages(list)
-    return ProcessPage(++index)
+    const list = await getPage(index);
+    if (!list.length) return;
+
+    const downloaded = await DownloadImages(list);
+
+    var dir = path.resolve("metadata");
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+    // write JSON string to a file
+    fs.writeFile(
+      `${dir}/page_${index}.json`,
+      JSON.stringify(downloaded, null, 2),
+      (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log("JSON data is saved.");
+      }
+    );
+
+    return ProcessPage(++index);
   }
 
-  return ProcessPage(process.env.PAGE_INDEX)
+  return ProcessPage(process.env.PAGE_INDEX);
 }
 
 Run().then(() => {
-  console.log("all done");
+  console.log("> DONE.");
 });
